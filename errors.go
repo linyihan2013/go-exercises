f23b46436c624e1eb9e3bab8f31fbd75de04520a
linyihan2013 <1032612468@qq.com>
2017-05-14 12:33:41 +0800

    2017/5/14

--- errors.go
+++ errors.go
@@ -13,7 +13,7 @@ func (e ErrNegativeSqrt) Error() string {
 
 func Sqrt(x float64) (float64, error) {
 	if x < 0 {
-		return x, ErrNegativeSqrt(x)
+		return 0, ErrNegativeSqrt(x)
 	}
 	return math.Sqrt(x), nil
 }
